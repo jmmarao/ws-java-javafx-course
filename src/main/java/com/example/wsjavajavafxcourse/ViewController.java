908ab240b5be8456721c2c57ab3dbc322ff3cc37
jones <jmmarao@gmail.com>
2024-02-21 17:09:03 -0300

    First event onClick

---
+++ src/main/java/com/example/wsjavajavafxcourse/ViewController.java
@@ -0,0 +1,15 @@
+package com.example.wsjavajavafxcourse;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ViewController {
+
+    @FXML
+    private Button btTest;
+
+    @FXML
+    public void onBtTestAction() {
+        System.out.println("Click");
+    }
+}
